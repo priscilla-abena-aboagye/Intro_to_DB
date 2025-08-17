25c23ad5891a8c28a129d06f062d43545d58e70c
Abena <priscillaabenaaboagye@gmail.com>
2025-08-17 17:20:54 +0000

    updated task 2

--- task_2.sql
+++ task_2.sql
@@ -1,76 +1,38 @@
-import mysql.connector
+USE alx_book_store;
+CREATE TABLE IF NOT EXISTS Authors (
+author_id INT AUTO_INCREMENT PRIMARY KEY,
+author_name VARCHAR(215));
 
-mydb = mysql.connector.connect(
-	host="localhost",
-	user="root",
-	password="WeOwnTheN!ght2025"
-)
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-print("Database creates successfully!!!")
 
-mydb = mysql.connector.connect(
-	host="localhost",
-	user="root",
-	password="WeOwnTheN!ght2025",
-	database="alx_book_store"
-)
-mycursor = mydb.cursor()
+CREATE TABLE IF NOT EXISTS Books (
+book_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(130),
+author_id INT,    
+price DOUBLE,
+publication_date DATE,
+FOREIGN KEY (author_id) REFERENCES Authors(author_id));
 
-mycursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215))
-    """
-)
+CREATE TABLE IF NOT EXISTS Customers (
+customer_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT);
 
-mycursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130),
-    author_id INT,    
-    price DOUBLE,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id))
-    """
-)
+CREATE TABLE IF NOT EXISTS Orders (
+order_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_id INT, 
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id));
 
-mycursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
-    address TEXT)
-    """
-)
+CREATE TABLE IF NOT EXISTS Order_Details (
+orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+order_id INT, 
+book_id INT,
+quantity DOUBLE,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id));
 
-mycursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT, 
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id))
-    """
-)
 
-mycursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS Order_Details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT, 
-    book_id INT,
-    quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id))
-    """
-)
-
-mydb.commit()
-mydb.close()
 
 
 
