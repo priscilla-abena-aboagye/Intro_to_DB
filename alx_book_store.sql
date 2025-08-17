97b32cc9c6337d9c235855f9c2f64758ab1f4a24
Abena <priscillaabenaaboagye@gmail.com>
2025-08-17 15:09:18 +0000

    foreign keys

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -64,9 +64,10 @@ mycursor.execute(
     order_id INT, 
     book_id INT,
     quantity DOUBLE),
-    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
     """
 )
+
 
 
