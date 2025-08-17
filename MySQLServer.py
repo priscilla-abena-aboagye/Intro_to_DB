09792d67510dfd698ac230c232f137784b023ff4
Abena <priscillaabenaaboagye@gmail.com>
2025-08-17 15:56:37 +0000

    made a change

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,9 +1,6 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
-    mydb = None
-    mycursor = None
     try:
         mydb = mysql.connector.connect(
             host="localhost",
@@ -14,7 +11,7 @@ def create_database():
         mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
 
     finally:
