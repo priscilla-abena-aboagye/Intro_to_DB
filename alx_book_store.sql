b07ffd2e114b955d916edd0bde4a23099d876049
Abena <priscillaabenaaboagye@gmail.com>
2025-08-17 15:07:31 +0000

    the foreign key

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -53,7 +53,7 @@ mycursor.execute(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT, 
     order_date DATE),
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
     """
 )
 
